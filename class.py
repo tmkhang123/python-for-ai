e0f63d46637a156dea9ade88cde41a1e3062a29a
Tran Minh Khang <khangdracm2004@gmail.com>
2026-05-03 16:40:24 +0700

    Intitial Commit

---
+++ class.py
@@ -0,0 +1,8 @@
+class Dog:
+    def __init__(self,name):
+        self.ten = name
+    def bark(self):
+        print("Bark!!!")
+Dog1 = Dog("Jerry")
+Dog1.ten
+Dog1.bark()
---
+++ hello.py
@@ -0,0 +1,6 @@
+first_name = "Minh"
+last_name = "Khang"
+full_name = first_name + ' ' +last_name
+long_dash = '-'
+ll_dash = long_dash * 13
+len(full_name)
