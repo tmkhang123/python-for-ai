8a4ce7cb0e238ac6a5205475dd7bb350b11a9250
Tran Minh Khang <khangdracm2004@gmail.com>
2026-05-03 17:01:11 +0700

    Remove comment

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-first_name = "Minh"
-last_name = "Khang"
-full_name = first_name + ' ' +last_name
-long_dash = '-'
-ll_dash = long_dash * 13
-len(full_name)
+import requests
+
+# We need coordinates to get weather data
+latitude = 48.85   # Paris latitude
+longitude = 2.35   # Paris longitude
+
+# Build the API URL with our parameters
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m"
+
+# Make the request
+response = requests.get(url)
+data = response.json()
+
+data.keys()
+data['current']['temperature_2m']
--- sales-analysis/analyzer.py
+++ sales-analysis/analyzer.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from helpers import calculate_total, format_currency
 
-# Read data
+
 df = pd.read_csv('data/sales.csv')
 
 # Calculate total for each row
@@ -24,4 +24,3 @@ for index, row in df.iterrows():
 grand_total = df['total'].sum()
 formatted_grand_total = format_currency(grand_total)
 print(f"\nGrand Total: {formatted_grand_total}")
-print(grand_total)
